c94cea40e5514989e487d7abfdd8c1747aea6aef
Jonathan Gac <jonathan.gac@icloud.com>
2020-02-09 22:58:12 +0100

    chore: init project

---
+++ src/app/root.jsx
@@ -0,0 +1,6 @@
+// @flow
+import * as React from 'react'
+
+const Root = () => <p>Hello world !</p>
+
+export default Root
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+import Root from './app/root'
+
+render(<Root />, document.getElementById('root'))

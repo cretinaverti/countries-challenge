c1e8d7d95599d1fda118adaaa1068007b4007da4
Jonathan Gac <jonathan.gac@icloud.com>
2020-02-23 23:14:04 +0100

    feat(app): add apollo client stuff

--- src/app/root.jsx
+++ src/app/root.jsx
@@ -1,6 +1,16 @@
 // @flow
 import * as React from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
 
-const Root = () => <p>Hello world !</p>
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+})
+
+const Root = () => (
+  <ApolloProvider client={client}>
+    <p>Hello world!</p>
+  </ApolloProvider>
+)
 
 export default Root

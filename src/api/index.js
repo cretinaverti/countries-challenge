7f1b80ac6c3602273001224c1165283da171d0a9
Jonathan Gac <jonathan.gac@icloud.com>
2020-02-23 21:19:33 +0100

    feat(api): init graphql server

---
+++ src/api/index.js
@@ -0,0 +1,17 @@
+import { ApolloServer } from 'apollo-server'
+import { schema } from './schema'
+import { dataSources } from './data-sources'
+
+const server = new ApolloServer({
+  schema,
+  dataSources,
+  tracing: true,
+})
+
+// The `listen` method launches a web server.
+server
+  .listen()
+  .then(({ url }) => {
+  // eslint-disable-next-line no-console
+    console.log(`🚀  Server ready at ${url}`)
+  })

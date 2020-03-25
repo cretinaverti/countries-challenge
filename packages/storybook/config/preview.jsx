d85a731f4a6a019c1af9880936f720a3ef1d0e3d
Jonathan Gac <jonathan.gac@icloud.com>
2020-03-25 20:28:49 +0100

    chore(storybook): add normalize.css

--- packages/storybook/config/preview.jsx
+++ packages/storybook/config/preview.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { addDecorator } from '@storybook/react'
 import { ThemeProvider } from 'theme-ui'
 import theme from 'theme'
+import 'normalize.css'
 
 addDecorator(storyFn => (
   <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>

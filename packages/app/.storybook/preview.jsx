7b0f6e35bbeba4eba6c17dd03e10e6da69e25c88
Jonathan Gac <jonathan.gac@icloud.com>
2020-03-26 22:43:37 +0100

    chore: add default padding to stories scenes
    Signed-off-by: Jonathan Gac <jonathan.gac@icloud.com>

--- packages/app/.storybook/preview.jsx
+++ packages/app/.storybook/preview.jsx
@@ -3,7 +3,10 @@ import { addDecorator } from '@storybook/react'
 import { ThemeProvider } from 'theme-ui'
 import theme from '../src/theme'
 import 'normalize.css'
+import { Box } from 'theme-ui'
 
 addDecorator(storyFn => (
-  <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
+  <ThemeProvider theme={theme}>
+    <Box p={3}>{storyFn()}</Box>
+  </ThemeProvider>
 ))

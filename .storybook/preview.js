cdc52c4221e41443f486909434c5d3baeaf3fc03
Jonathan Gac <jonathan.gac@icloud.com>
2021-02-16 21:01:01 +0100

    chore: add apollo client mock to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,9 @@
+import { MockedProvider } from '@apollo/client/testing'
 import '../styles/globals.css'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  apolloClient: {
+    MockedProvider,
+  },
 }
